a9a0680529038b4bd3012f26a876edcf169f561f
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2022-09-05 23:16:29 -0300

    feat: criada a rota o controller e as views de criação e exibição do model Produto

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Produto;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $produtos = DB::select(" 
+        SELECT produtos.id, produtos.nome, produtos.preco, tipo_produtos.descricao
+        FROM restaurantedb.produtos
+        JOIN restaurantedb.tipo_produtos 
+        ON produtos.Tipo_Produtos_id = tipo_produtos.id;"
+        );
+
+
+        return view("Produto/index")->with("produtos", $produtos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $tipoProdutos = DB::select(" 
+        SELECT id, descricao
+        FROM  restaurantedb.tipo_produtos;"
+        );
+
+        return view("Produto/create")->with("tipoProdutos", $tipoProdutos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produto = new Produto();
+        $produto->nome = $request->nome;
+        $produto->preco = $request->preco;
+        $produto->Tipo_Produtos_Id = $request->tipo;
+        $produto->ingredientes = $request->ingredientes;
+        $produto->urlImage = $request->urlDaImagem;
+        $produto->save();
+        return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/Produto/create.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Produto</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <form action="/produto" method="post">
+            @csrf
+            <div class="form-group">
+              <label for="id-input-id">ID</label>
+              <input type="text" class="form-control" id="id-input-id" aria-describedby="idHelp" placeholder="#" disabled>
+              <small id="idHelp" class="form-text text-muted">Não é necessário cadastrar um ID para um novo dado.</small>
+            </div>
+            <div class="form-group">
+              <label for="id-input-nome">Nome</label>
+              <input name="nome" type="text" class="form-control" id="id-input-nome" placeholder="Digite o nome do produto">
+            </div>
+            <div class="form-group">
+                <label for="id-input-preco">Preço</label>
+                <input name="preco" type="number" class="form-control" id="id-input-preco" placeholder="Digite o preço do produto" step=".01">
+            </div>
+            <div class="form-group">
+                <label for="id-select-tipo">Tipo</label>
+                <select name="tipo" id="id-select-tipo" class="form-select" aria-label="Selecione um tipo" required="required">
+                    <option value="-1"></option>
+                    <@foreach ($tipoProdutos as $tipo)
+                    <option value="{{$tipo->id}}">{{$tipo->descricao}}</option>
+                    @endforeach
+                </select>
+                <!--<input name="tipo" type="number" class="form-control" id="id-input-tipo" placeholder="Digite o tipo do produto">-->
+            </div>
+            <div class="form-group">
+                <label for="id-input-ingredientes">Ingredientes</label>
+                <input name="ingredientes" type="text" class="form-control" id="id-input-ingredientes" placeholder="Digite os Ingredientes do produto">
+            </div>
+            <div class="form-group">
+                <label for="id-input-ingredientes">Url da Imagem</label>
+                <input name="urlDaImagem" type="text" class="form-control" id="id-input-urlDaImagem" placeholder="Digite a Url da Imagem">
+            </div>
+            <div class="my-1">
+                <button  type="submit" class="btn btn-primary">Submit</button>
+                <a class="btn btn-primary" href="/produto">Voltar</a>
+            </div>
+        </form>
+    </div>   
+</body>
+</html>
